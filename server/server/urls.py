991a376736e50ddd6622947d713f61cebb565c8d
Arven <brian.johnathan.b@gmail.com>
2014-07-18 09:51:22 -0400

    Add some CRUD URLs to Django.

--- server/server/urls.py
+++ server/server/urls.py
@@ -5,8 +5,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'server.views.home', name='home'),
+    # url(r'^$', 'django_inventory.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^django/inventory/$', 'inventory.views.index'),
+    url(r'^django/inventory/(?P<item_id>[a-zA-Z0-9-]+)/$', 'inventory.views.detail'),
+    url(r'^django/update/inventory/(?P<item_id>[a-zA-Z0-9-]+)/(?P<new_item_count>\d+)/$', 'inventory.views.updatecount'),
+    url(r'^django/delete/inventory/(?P<item_id>[a-zA-Z0-9-]+)/$', 'inventory.views.delete'),
+    url(r'^django/create/inventory/$', 'inventory.views.create'),
+    url(r'^django/admin/', include(admin.site.urls)),
 )
