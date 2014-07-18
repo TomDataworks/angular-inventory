dc41177c9f1c3f3382951ea3b9c292a847c743a5
Arven <brian.johnathan.b@gmail.com>
2014-07-18 19:15:40 -0400

    Change the server urls around, for disambiguation.

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from accounts import views as views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,5 +14,7 @@ urlpatterns = patterns('',
     url(r'^django/update/inventory/(?P<item_id>[a-zA-Z0-9-]+)/(?P<new_item_count>\d+)/$', 'inventory.views.updatecount'),
     url(r'^django/delete/inventory/(?P<item_id>[a-zA-Z0-9-]+)/$', 'inventory.views.delete'),
     url(r'^django/create/inventory/$', 'inventory.views.create'),
+    url(r'^django/accounts/users/$', views.UserView),
+    url(r'^django/accounts/auth/$', views.AuthView.as_view(), name='authenticate'),
     url(r'^django/admin/', include(admin.site.urls)),
 )
