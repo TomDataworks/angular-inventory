875938f48fbbd7dc1d2e11cdb391841aae2ce7f3
Arven <brian.johnathan.b@gmail.com>
2014-07-18 18:56:04 -0400

    Update models and change paths for authentication system in Django.

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -48,11 +48,11 @@ factory('api', function($resource){
   // we tell Django not to [3]. This is a problem as the POST data cannot
   // be sent with the redirect. So we want Angular to not strip the slashes!
   return {
-    auth: $resource('/api/auth\\/', {}, {
+    auth: $resource('/django/auth\\/', {}, {
       login: {method: 'POST', transformRequest: add_auth_header},
       logout: {method: 'DELETE'}
     }),
-    users: $resource('/api/users\\/', {}, {
+    users: $resource('/django/users\\/', {}, {
       create: {method: 'POST'}
     })
   };
