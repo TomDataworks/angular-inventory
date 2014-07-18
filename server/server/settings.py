1cb1500cd453dbfd5534c022f51de4d4aa11747a
Arven <brian.johnathan.b@gmail.com>
2014-07-18 19:06:36 -0400

    Added the authentication application to the server settings. We want to keep authentication separated from the main application.

--- server/server/settings.py
+++ server/server/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory'
+    'inventory',
+    'accounts'
 )
 
 MIDDLEWARE_CLASSES = (
