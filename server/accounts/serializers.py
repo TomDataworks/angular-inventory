2bb4d373554cc62f3c55840cc0b4ecaf93fe1961
Arven <brian.johnathan.b@gmail.com>
2014-07-18 19:02:32 -0400

    Add the serialization functions for the User objects.

---
+++ server/accounts/serializers.py
@@ -0,0 +1,21 @@
+import HTMLParser
+import random
+import re
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from . import models
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'first_name',
+                  'last_name', 'email')
+        read_only_fields = ('id',)
+        write_only_fields = ('password',)
+
+    def restore_object(self, attrs, instance=None):
+        user = super(UserSerializer, self).restore_object(attrs, instance)
+        user.set_password(attrs['password'])
+        return user
