0655b898480c33b6f7b6f54d0aaa7821a4c28fe9
Arven <brian.johnathan.b@gmail.com>
2014-07-19 17:43:36 -0400

    Add Gruntfile and index.html changes to allow concatted javascript.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,14 @@ module.exports = function(grunt) {
       app: {
         src: ['src/client/app/js/*.js'],
         dest: 'dist/client/app/js/app.js'
+      },
+      lib: {
+        src: ['src/client/app/bower_components/angular/angular.js',
+              'src/client/app/bower_components/angular-route/angular-route.js',
+              'src/client/app/bower_components/angular-cookies/angular-cookies.js',
+              'src/client/app/bower_components/angular-resource/angular-resource.js',
+              'src/client/app/bower_components/autofill-event/src/autofill-event.js'],
+        dest: 'dist/client/app/js/lib.js'
       }
     }
   });
