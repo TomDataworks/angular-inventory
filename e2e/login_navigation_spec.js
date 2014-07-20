1fa4a1f199f5952eeafea91292f4a319c38696ca
Arven <brian.johnathan.b@gmail.com>
2014-07-20 18:46:55 -0400

    Remove some useless navigation commands, side effect is if nav doesn't work like expected it will throw errors which is desired.

--- e2e/login_navigation_spec.js
+++ e2e/login_navigation_spec.js
@@ -16,8 +16,8 @@ expect(loginform.isDisplayed()).toEqual(true);
 });
 
 it("should allow the user to login", function() {
-ptor.get("/client/app/index.html#/products");
-ptor.driver.executeScript("$.fx.off = true;");
+//ptor.get("/client/app/index.html#/products");
+//ptor.driver.executeScript("$.fx.off = true;");
 element(by.model("username")).sendKeys('arven');
 element(by.model("password")).sendKeys('testing');
 element(by.buttonText("Sign in")).click();
@@ -25,23 +25,23 @@ ptor.sleep(1000);
 });
 
 it("should display the logout form when the user is logged in", function() {
-ptor.get("/client/app/index.html#/products");
-ptor.driver.executeScript("$.fx.off = true;");
+//ptor.get("/client/app/index.html#/products");
+//ptor.driver.executeScript("$.fx.off = true;");
 var logoutform;
 logoutform = element(by.id("logoutform"));
 expect(logoutform.isDisplayed()).toEqual(true);
 });
 
 it("should allow the user to logout", function() {
-ptor.get("/client/app/index.html#/products");
-ptor.driver.executeScript("$.fx.off = true;");
+//ptor.get("/client/app/index.html#/products");
+//ptor.driver.executeScript("$.fx.off = true;");
 element(by.partialButtonText("logout")).click();
 ptor.sleep(1000);
 });
 
 it("should display the login form when the user is logged out again", function() {
-ptor.get("/client/app/index.html#/products");
-ptor.driver.executeScript("$.fx.off = true;");
+//ptor.get("/client/app/index.html#/products");
+//ptor.driver.executeScript("$.fx.off = true;");
 var loginform;
 loginform = ptor.findElement(protractor.By.id("loginform"));
 expect(loginform.isDisplayed()).toEqual(true);
