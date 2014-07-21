4bbd305efb75ea6568969f8171a051b8bfb4a394
Arven <brian.johnathan.b@gmail.com>
2014-07-21 19:41:17 -0400

    Now test for updating an item.

--- server/inventory/tests.py
+++ server/inventory/tests.py
@@ -33,3 +33,13 @@ class InventoryItemTestCase(TestCase):
         request = factory.post('/django/inventory/', {'fields': {'itemId': 'product-b', 'name': 'Product B', 'count':12, 'short': 'Test product description 2', 'desc': 'Test product long description 2'}}, format='json')
         product_b = Item.objects.filter(itemId="product-b")
         self.assertFalse(product_b.exists())
+
+    # Now we try to update an item, but don't authenticate ourselves. Thus, we
+    # should get the initial item
+    def test_inventory_update_item_unauthenticated(self):
+        """Inventory items not added when we are not authenticated"""
+        request = factory.post('/django/inventory/product-a', {'fields': {'itemId': 'product-a', 'name': 'Product A', 'count':120, 'short': 'Test product description', 'desc': 'Test product long description'}}, format='json')
+        product_a = Item.objects.filter(itemId="product-a")
+        self.assertTrue(product_a.exists())
+        first = product_a[0]
+        self.assertEqual(first.count, 12)
