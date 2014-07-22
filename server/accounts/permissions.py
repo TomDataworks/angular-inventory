0701a6a86566e789ec5130245810c27e80369730
Arven <brian.johnathan.b@gmail.com>
2014-07-21 20:50:17 -0400

    Delete the permissions.py, this is extraneous for this application.

--- server/accounts/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions
-
-# Determine if a user has Staff permissions, this isn't really
-# used in our trivial example
-class IsStaff(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return view.action == 'retrieve' or request.user.is_staff
